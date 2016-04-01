012d3ca2a5eb91c79541a01b0ed780741bc958bc
juhish <juhish@users.noreply.github.com>
2016-04-01 17:18:03 -0400

    Added files via upload

---
+++ QuoteService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Homesite.ECommerce.Context;
+using Homesite.Diagnostics.ECommerce;
+using Homesite.ECommerce.WebServices.QuoteServicesRef;
+
+namespace Homesite.ECommerce.WebServices
+{
+    public class QuoteService : WebServicesClientBase<QuoteServicesClient, QuoteServices>
+    {
+        public QuoteService()
+        {
+        }
+
+        public void ProcessPartialMatchModal(Dictionary<string, string> qHeader)
+        {
+            try
+            {
+                this.GetQuoteHeaderServiceClient(qHeader).ProcessPartialMatchModal(qHeader);
+            }
+            finally
+            {
+                this.CloseServiceClient();
+            }
+        }
+
+        public void ProcessNoMatchModal(Dictionary<string, string> qHeader)
+        {
+            try
+            {
+                this.GetQuoteHeaderServiceClient(qHeader).ProcessNoMatchModal(qHeader);
+            }
+            finally
+            {
+                this.CloseServiceClient();
+            }
+        }
+
+
+    }
+}
